6545bfbc668cdb6b844eae37ae063bcd3d0848db
RAFAEL DE OLIVEIRA PEIXOTO MEDEIROS <a92328933@rn.senac.local>
2024-12-18 18:42:19 -0300

    add: repo adicional

---
+++ controller/controller.js
@@ -0,0 +1,4 @@
+const {pessoas} = require("../database/database")       //Aqui estou importando o banco (pessoas) que foi criado na pasta database.
+const {produtos} = require("../database/database")       //Aqui estou importando o banco (produtos) que foi criado na pasta database
+
+console.log(pessoas)
