3dfe9d0ebc1e2a865895500b67d3da5cf31fd005
Rafael Medeiros <oliver.rafadev@gmail.com>
2024-12-19 21:33:20 -0300

    fix: try catch no crud

--- controller/controller.js
+++ controller/controller.js
@@ -8,7 +8,19 @@ console.log(pessoas)
 
 //Listando usuarios
 function listar(){
-    console.log(pessoas)
+    try {
+        if(pessoas.length <= 0){
+            console.log("Nenhum usuário cadastrado!")
+        }else{
+            console.log("Usuarios:")
+            console.table(pessoas);
+        }
+        
+    } catch (error) {
+        console.error("Erro ao listar os usuarios.", error.message)
+    }
+
+
 }
 
 
@@ -21,6 +33,7 @@ function cadastrar (id, nome, telefone, email) {    //parametros para ser inseri
         email
     };
     pessoas.push(novoUsuario)   //Envia os dados do novoUsuario para o "banco" pessoas
+    console.log('Usuario cadastrado com sucesso!')
 }
 
 //Editar
@@ -31,19 +44,27 @@ function editar(id, novoNome, novoTelefone, novoEmail){ //novos parametros atual
         pessoa.nome = novoNome || pessoa.nome;
         pessoa.telefone = novoTelefone || pessoa.telefone;
         pessoa.email = novoEmail || pessoa.email
+        console.log('Usuário atualizado com sucesso!')
     } else {
-        console.log('Usuário não encontrado!')
+        console.log('Usuario não encontrado!')
     }
 }
 
 //Deletar todos
 function deletarTodos(){
-    pessoas = [];   //Torna o banco vazio.
+    pessoas.length = 0;   //Torna o banco vazio.
+    
 }
 
 //Deletar por id
-function deletarPorId () {
-    
+function deletarPorId (id) {
+    const usuario = pessoas.findIndex(elemento => elemento.id === id)   //pego o indice do array onde está o id
+    if(usuario === -1){
+        console.log('Usuário não encontrado')
+    }else{
+        pessoas.splice(usuario, 1)
+        console.log('Usuário deletado com sucesso!')
+    }
 }
 
 
@@ -54,4 +75,7 @@ cadastrar(2, "Rafael", "(84)998873257", "rafael@gmail.com")
 
 editar(1, "Roberto", "(84)996783254", "roberto@email.com")
 
+deletarPorId(1)
+listar()
+deletarTodos()
 listar()
